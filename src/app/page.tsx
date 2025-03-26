f5d7202bddf3a64d02fc17ec570597d9a7ff4a0c
Phumsirii <phumsirisumativit@gmail.com>
2025-03-26 15:55:17 +0700

    styling: contact-us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className="max-h-screen max-w-screen">
       {/* Grid container */}
       <div className="grid grid-cols-9 h-12 border-b m-10 text-m">
-        <div className="col-span-5 flex justify-start p-2 text-2xl text-bold">HOME</div>
+        <div className="col-span-5 flex justify-start p-2 text-2xl text-bold">
+          HOME
+        </div>
         <div className="flex justify-start p-2">TICKET</div>
         <div className="flex justify-start p-2">INFO</div>
         <div className="flex justify-start p-2">CONTACT</div>
         <div className="flex justify-start p-2">MEDIA</div>
       </div>
-      <div className="flex flex-col py-40 h-full justify-start text-7xl text-center items-center text-3xl">
-            <h1 className="font-zahrah-thai">CRIM(E)SON</h1>
-            <h1 className="font-sans-serif">HOUSE</h1>
+      <div className="flex flex-col py-40 h-full justify-start text-7xl text-center items-center">
+        <h1 className="font-zahrah-thai">CRIM(E)SON</h1>
+        <h1 className="font-sans-serif">HOUSE</h1>
       </div>
-
     </div>
   );
 }
