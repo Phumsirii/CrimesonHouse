4f5ff862f7e244253064aa202ae1eb5bedb5d914
Phumsirii <phumsirisumativit@gmail.com>
2025-03-26 22:11:26 +0700

    feat: media page

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,3 +1,20 @@
+import MediaCard from "./media-card";
+
+const mockMedia = [
+  {
+    imageUrl: "/image/image.png",
+    description: "Image 1",
+  },
+  {
+    imageUrl: "/image/image.png",
+    description: "Image 2",
+  },
+  {
+    imageUrl: "/image/image.png",
+    description: "Image 3",
+  },
+];
+
 export default function MediaPage() {
   return (
     <div className="max-h-screen max-w-screen flex flex-col lg:px-40 md:px-32 sm:px-20 px-4 space-y-10 lg:space-y-20">
@@ -5,7 +22,11 @@ export default function MediaPage() {
         MEDIA
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-10">
+        {mockMedia.map((media, i) => (
+          <MediaCard key={i} media={media} />
+        ))}
+      </div>
     </div>
   );
 }
