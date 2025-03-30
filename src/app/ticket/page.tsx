4775c312e6d85105a79dd5e423d97805b9bb3a18
ThitipatC <thitipatchaiyakul@gmail.com>
2025-03-30 16:09:07 +0800

    feat: init ticket page

---
+++ src/app/ticket/page.tsx
@@ -0,0 +1,10 @@
+export default function Ticket() {
+    return (
+      <div className="h-screen w-screen py-10 px-30 flex items-center justify-center">
+        <div className="flex bg-[#F2D6B3] flex-col border w-full h-full justify-start text-center items-center text-5xl py-10">
+              <h1 className="font-zahrah-thai">Ticket</h1>
+        </div>
+      </div>
+    );
+  }
+  
