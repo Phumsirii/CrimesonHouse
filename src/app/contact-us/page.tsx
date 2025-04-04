e648d94553b5e6987e08eda1c11d2f55a9cca017
Phumsirii <phumsirisumativit@gmail.com>
2025-04-04 22:22:09 +0700

    chore: contact us styling

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -4,54 +4,48 @@ import Link from "next/link";
 export default function ContactUsPage() {
   return (
     <div className="bg-[url('/image/bg-statue.png')] text-white bg-cover bg-center h-screen w-screen flex flex-col py-24 lg:px-40 md:px-32 sm:px-20 px-4 space-y-12 lg:space-y-24">
-      <div className="flex justify-center p-2 sm:text-7xl text-5xl font-bold lg:mt-10">
+      <div className="flex justify-center p-2 sm:text-5xl text-3xl font-bold lg:mt-10">
         CONTACT US
       </div>
-      <div className="flex flex-row text-xl lg:text-2xl">
-        <div className="flex flex-col space-y-10 lg:space-y-16">
-          <Link
-            className="bg-[linear-gradient(25deg,_#feda75,_#fa7e1e,_#d62976,_#962fbf,_#4f5bd5)] flex flex-row space-x-2 lg:space-x-8 bg-black/60 items-center shadow-2xl hover:-translate-y-0.5 delay-100 transform transition-transform duration-200 rounded-2xl p-2"
-            href={
-              "https://www.instagram.com/crimesonhouse?igsh=d2wzNzNxemVibTBl&utm_source=qr"
-            }
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon icon="mdi:instagram" className="w-12 lg:w-20 h-full" />
-            <div className="flex flex-col">
-              <div className="text-4xl lg:text-5xl">&#64;crimesonhouse</div>
-            </div>
-          </Link>
 
-          <Link
-            className="bg-[linear-gradient(25deg,_#69C9D0,_#000000,_#EE1D52)] flex flex-row space-x-2 lg:space-x-8 bg-black/60 items-center shadow-2xl hover:-translate-y-0.5 delay-100 transform transition-transform duration-200 rounded-2xl p-2"
-            href={
-              "https://www.tiktok.com/@crimesonhouse?_t=ZS-8vCfJXlsCQs&_r=1"
-            }
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon
-              icon="akar-icons:tiktok-fill"
-              className="w-12 lg:w-20 h-full"
-            />
-            <div className="flex flex-col">
-              <div className="text-4xl lg:text-5xl">crimesonhouse</div>
-            </div>
-          </Link>
+      <div className="flex flex-row flex-wrap gap-10 justify-center">
+        <Link
+          className="bg-[linear-gradient(25deg,_#feda75,_#fa7e1e,_#d62976,_#962fbf,_#4f5bd5)] flex flex-row space-x-2 lg:space-x-8 items-center shadow-2xl hover:-translate-y-0.5 delay-100 transform transition-transform duration-200 rounded-2xl p-2 w-80 justify-center"
+          href={
+            "https://www.instagram.com/crimesonhouse?igsh=d2wzNzNxemVibTBl&utm_source=qr"
+          }
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon icon="mdi:instagram" className="w-6 lg:w-9 h-full" />
+          <div className="flex flex-col">
+            <div className="text-xl lg:text-3xl">&#64;crimesonhouse</div>
+          </div>
+        </Link>
 
-          <Link
-            className="bg-[#06C755] flex flex-row space-x-2 lg:space-x-8 items-center shadow-2xl hover:-translate-y-0.5 delay-100 transform transition-transform duration-200 rounded-2xl p-2"
-            href={"https://lin.ee/Zm0GY3d"}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Icon icon="ri:line-fill" className="w-12 lg:w-20 h-full" />
-            <div className="flex flex-col">
-              <div className="text-4xl lg:text-5xl">&#64;CrimesonHouse</div>
-            </div>
-          </Link>
-        </div>
+        <Link
+          className="bg-[linear-gradient(25deg,_#69C9D0,_#000000,_#EE1D52)] flex flex-row space-x-2 lg:space-x-8 items-center shadow-2xl hover:-translate-y-0.5 delay-100 transform transition-transform duration-200 rounded-2xl p-2 w-80 justify-center"
+          href={"https://www.tiktok.com/@crimesonhouse?_t=ZS-8vCfJXlsCQs&_r=1"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon icon="akar-icons:tiktok-fill" className="w-6 lg:w-9 h-full" />
+          <div className="flex flex-col">
+            <div className="text-xl lg:text-3xl">crimesonhouse</div>
+          </div>
+        </Link>
+
+        <Link
+          className="bg-[#06C755] flex flex-row space-x-2 lg:space-x-8 items-center shadow-2xl hover:-translate-y-0.5 delay-100 transform transition-transform duration-200 rounded-2xl p-2 w-80  justify-center"
+          href={"https://lin.ee/Zm0GY3d"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Icon icon="ri:line-fill" className="w-6 lg:w-9 h-full" />
+          <div className="flex flex-col">
+            <div className="text-xl lg:text-3xl">&#64;CrimesonHouse</div>
+          </div>
+        </Link>
       </div>
     </div>
   );
