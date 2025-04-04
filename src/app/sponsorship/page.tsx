a11b4a3cc7e24ddcad588563cbe0bc061b626a03
ThitipatC <thitipatchaiyakul@gmail.com>
2025-04-04 19:14:47 +0800

    add contact for sponsorship interest

--- src/app/sponsorship/page.tsx
+++ src/app/sponsorship/page.tsx
@@ -59,6 +59,15 @@ export default function SponsorshipPage() {
                 {otherSponsors.slice(5).map(logo => <Image alt = "logo" key = {logo} width= {100} className="bg-white" height={100} src={`/image/others/${logo}`}/>)}
         </div>
       </div>
+      <div className="fixed bottom-20 left-20 p-2 bg-opacity-50 text-white">
+      <p>สนใจสนับสนุนโครงการ สามารถติดต่อได้ที่<br />
+          Instagram: @crimesonhouse <br />
+          Email: whoisthemurderproject@gmail.com<br />
+          Tel: 062-042-7236 (เบส ฝ่ายจัดหาทุน)<br />
+        </p>
+      </div>
+        
+        
     </div>
 
 
