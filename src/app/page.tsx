ad4d86be2887033da1f2bb5fe4b0fce416a74f92
ThitipatC <thitipatchaiyakul@gmail.com>
2025-04-04 10:43:02 +0800

    add bg for teaser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="min-h-screen bg-black text-white">
        
       {/* Hero Section */}
-      <section className="relative h-screen  border">
+      <section className="relative h-screen">
         <div className="relative h-full">
           <Image
             src="/image/GFXP0640.jpg"
@@ -16,7 +16,7 @@ export default function Home() {
             style={ {objectFit: "cover", objectPosition: "50% 36%" }}
           />
         </div>
-        <div className="absolute top-70 left-100 flex flex-col items-center justify-center text-center px-4">
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
           <h1 className="text-4xl md:text-6xl font-extrabold mb-4 tracking-wider">CRIM(E)SONE</h1>
           <h1 className="text-4xl md:text-6xl font-extrabold mb-4 tracking-wider">HOUSE</h1>
           <h2 className="text-xl md:text-2xl font-light mb-8 max-w-3xl">2-4 May 2025</h2>
@@ -28,10 +28,11 @@ export default function Home() {
       </section>
 
       {/* Show Info Section */}
-      <section className="bg-zinc-900 py-20">
+      <section className="py-20 bg-[url('/image/BG-red-retouch.png')]">
         <div className="flex flex-col mx-auto">
+  
           <h2 className="text-3xl md:text-4xl font-bold text-center text-amber-700 mb-16 relative after:content-[''] after:absolute after:w-20 after:h-1 after:bg-amber-700 after:bottom-[-15px] after:left-1/2 after:transform after:-translate-x-1/2">Teaser</h2>
-          <video className='border '>
+          <video className=''>
           </video>
         </div>
       </section>
