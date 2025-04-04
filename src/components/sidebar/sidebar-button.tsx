2385d39bf87af31a5c87402e9175fb0e7cdd1fe2
Phumsirii <phumsirisumativit@gmail.com>
2025-04-04 23:21:24 +0700

    styling: sidebar font

--- src/components/sidebar/sidebar-button.tsx
+++ src/components/sidebar/sidebar-button.tsx
@@ -13,7 +13,7 @@ export function SidebarButton({ title, link, handleOnClick }: MenuProps) {
     <button onClick={handleOnClick} className="w-full">
       <Link
         href={link}
-        className="flex w-full cursor-pointer items-center justify-start space-x-3 p-2 text-primary"
+        className="flex w-full cursor-pointer items-center justify-start space-x-3 p-2 text-white"
       >
         <span>{title}</span>
       </Link>
