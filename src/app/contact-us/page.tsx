4e11bd66e9718bb62dc9105d49ffd09d1ba40a7f
Phumsirii <phumsirisumativit@gmail.com>
2025-04-04 20:17:52 +0700

    styling: text colour

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function ContactUsPage() {
   return (
-    <div className="max-h-screen max-w-screen flex flex-col py-24 lg:px-40 md:px-32 sm:px-20 px-4 space-y-4 lg:space-y-10 ">
+    <div className="bg-[url('/image/red_bg.jpg')] text-white bg-cover bg-center h-screen w-screen flex flex-col py-24 lg:px-40 md:px-32 sm:px-20 px-4 space-y-4 lg:space-y-10 ">
       <div className="flex justify-center p-2 sm:text-7xl text-5xl font-bold lg:mt-10">
         CONTACT US
       </div>
       <div className="flex flex-row text-xl lg:text-2xl">
-        <div className="flex flex-col space-y-10 lg:space-y-16 text-white">
+        <div className="flex flex-col space-y-10 lg:space-y-16">
           <Link
             className="flex flex-row space-x-2 lg:space-x-8 bg-black/60 items-center shadow-2xl hover:-translate-y-0.5 delay-100 transform transition-transform duration-200 rounded-2xl p-2"
             href={
