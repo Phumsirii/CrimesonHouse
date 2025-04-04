a12fbde647aaf0521008a76798cb01d69341b10f
Phumsirii <phumsirisumativit@gmail.com>
2025-04-04 23:32:32 +0700

    feat: font changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TopBar from "@/components/top-bar";
+import { Great_Vibes } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const greatVibes = Great_Vibes({
   subsets: ["latin"],
+  weight: "400",
+  variable: "--font-great-vibes",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,12 +31,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={greatVibes.variable}>
       <head>
         <title>CrimesonHouse</title>
-        <link rel="icon" href="/image/LOGO-t-01.png" />
+        <link rel="icon" href="/image/logo.png" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="font-primary">
         <TopBar />
 
         {children}
