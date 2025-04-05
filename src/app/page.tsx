0d89f7db732fb3226a31a46d3f5df8cd810c230f
ThitipatC <thitipatchaiyakul@gmail.com>
2025-04-05 10:38:36 +0800

    add photo layout for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,8 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
       {/* Hero Section */}
-      <section className="relative h-screen">
-        <div className="relative h-full">
-          <Image
-            src="/image/GFXP0640.jpg"
-            alt="Poster"
-            fill={true}
-            style={{ objectFit: "cover", objectPosition: "50% 36%" }}
-          />
-        </div>
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
+      <section className="w-screen bg-cover bg-center h-screen  py-50 border lg:h-screen lg:border-none lg:bg-cover lg:bg-[position:20%_40%] bg-[url('/image/GFXP0640.jpg')]">
+        <div className="flex flex-col items-center h-full justify-center text-center px- ">
           <h1 className="text-4xl md:text-6xl font-extrabold mb-4 tracking-wider">
             CRIM(E)SONE
           </h1>
@@ -39,11 +31,17 @@ export default function Home() {
 
       {/* Show Info Section */}
       <section className="py-20 bg-[url('/image/BG-red-retouch.png')]">
-        <div className="flex flex-col mx-auto">
+        <div className="flex flex-col mx-auto items-center px-5">
           <h2 className="text-3xl md:text-4xl font-bold text-center text-amber-700 mb-16 relative after:content-[''] after:absolute after:w-20 after:h-1 after:bg-amber-700 after:bottom-[-15px] after:left-1/2 after:transform after:-translate-x-1/2">
             Teaser
           </h2>
-          <video className=""></video>
+            <div className="mx-5 w-full max-w-4xl aspect-video">
+              <iframe 
+                src="https://drive.google.com/file/d/1bvvy8iP2ISLK_M3_oYhCtG_fikGPS89g/preview" 
+                className=" w-full h-full origin-center rounded-lg shadow-lg"
+                allowFullScreen
+              ></iframe>
+            </div>
         </div>
       </section>
     </div>
