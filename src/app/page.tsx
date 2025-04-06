ddb81cf63ad42931e4548d7db6fa48905d792895
Phumsirii <phumsirisumativit@gmail.com>
2025-04-06 11:40:44 +0700

    fix: add eng text for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,53 @@
+"use client";
+
+import { useAppSelector } from "@/redux/store";
 import Link from "next/link";
-import Image
- from "next/image";
+import Image from "next/image";
 export default function Home() {
+  const language = useAppSelector((state) => state.languageSlice.language);
+
   return (
     <div className="min-h-screen bg-black text-white">
       {/* Hero Section */}
       <section className="w-screen bg-cover bg-center h-screen  py-50 lg:h-screen lg:border-none lg:bg-cover lg:bg-[position:20%_32%] bg-[url('/image/GFXP0640.jpg')]">
         <div className="flex flex-col items-center h-full justify-center text-center px- ">
-          <Image alt="title" src="/image/ชื่อเรื่อง.png" width={800} height = {800}/>
+          <Image
+            alt="title"
+            src="/image/ชื่อเรื่อง.png"
+            width={800}
+            height={800}
+          />
           <h2 className="text-3xl md:text-5xl font-light mb-8 max-w-3xl">
             2-4 May 2025
           </h2>
           <h3 className="md:hidden text-xl sm:text-xl italic font-light mb-8 text-center">
-            <q>ความจริงทุกอย่าง อยู่ในบ้านหลังนั้น <br/>
-            คุณต้องไปพิสูจน์ด้วยตัวคุณเอง</q>
+            {language === "TH" ? (
+              <q>
+                ความจริงทุกอย่าง อยู่ในบ้านหลังนั้น <br />
+                คุณต้องไปพิสูจน์ด้วยตัวคุณเอง
+              </q>
+            ) : (
+              <q>
+                The truth lies within that house;
+                <br />
+                now it is your turn to uncover it.
+              </q>
+            )}
           </h3>
-    
-        <h3 className="hidden md:block text-2xl italic font-light mb-8 text-center">
-          <q>ความจริงทุกอย่าง อยู่ในบ้านหลังนั้น คุณต้องไปพิสูจน์ด้วยตัวคุณเอง</q>
-        </h3>
+
+          <h3 className="hidden md:block text-2xl italic font-light mb-8 text-center">
+            {language === "TH" ? (
+              <q>
+                ความจริงทุกอย่าง อยู่ในบ้านหลังนั้น
+                คุณต้องไปพิสูจน์ด้วยตัวคุณเอง
+              </q>
+            ) : (
+              <q>
+                The truth lies within that house; now it is your turn to uncover
+                it.
+              </q>
+            )}
+          </h3>
           <Link
             href="/tickets"
             className="bg-amber-700 hover:bg-amber-600 text-black font-bold py-4 px-8 rounded uppercase text-lg tracking-wider transition-colors"
